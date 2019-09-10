22ed5516149641cae4100e9cd74488ccc80feff3
joanne282 <joanne@Joanneui-MacBook-Pro.local>
2019-09-10 21:01:17 +0900

    Create 9_5.cpp

---
+++ dojang/dojang/9_5.cpp
@@ -0,0 +1,20 @@
+//
+//  9_5.cpp
+//  dojang
+//
+//  Created by Joanne Leem on 10/09/2019.
+//  Copyright © 2019 Joanne Leem. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    char c1 = '\t';
+    
+    printf("Hello");
+    printf("%c", c1);
+    printf("world\n");
+    
+    return 0;
+}
